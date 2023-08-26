4a2548646e12ca7c85b8faace853b078fd7a6476
faby06 <143236825+faby06@users.noreply.github.com>
2023-08-26 12:15:33 -0600

    Add files via upload

---
+++ 1_Variables_Strings_Concatenar_Upper_Lower_Title_SaltoLinea_Tabu.py
@@ -0,0 +1,22 @@
+#Crea una Variable y String 
+a="Fabiola" #Esta variable es de tipo string y se creo con las comillas dobles pero tambien se pueden simples 
+print(a) #Se manda imprimir lo que tiene en la varible a
+
+#Concatenar Strings 
+b="Fabiola"
+c="Castro"
+d="Hdez"
+concatenar = b+" "+c+" "+d # Para juntar estos strings se utiliza el +  
+print(concatenar)
+
+#Metodos upper,lower,title
+nombre = "inteligencia"
+nombre = nombre.upper() #Toda la frase la convierte a mayusculas usando el metodo upper
+print(nombre)
+nombre1= "ARTIFICIAL"
+print(nombre1.lower()) #Hace lo contrario a upper convierte toda la frase a minusculas
+
+#Salto de linea y tabulacion
+print("Hola\nMundo") #Estoy mando imprimir esta frase y para mundo me salga en la parte de abajo de hola uso este simbolo para saltar linea \n
+print("Hola\tMundo2")#Estoy haciendo lo mismo que en print anterior solo que ahora para tabular uso\t
+
