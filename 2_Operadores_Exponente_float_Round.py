0097152ce1f2c9343b4ddf20b005ff2ebc29704e
faby06 <143236825+faby06@users.noreply.github.com>
2023-08-26 12:16:03 -0600

    Add files via upload

---
+++ 2_Operadores_Exponente_float_Round.py
@@ -0,0 +1,16 @@
+#Operadores
+suma  = 25+15   
+resta = 10-5
+multi = 3*6
+divi  = 8/4
+print (suma,resta,multi,divi) #Se declaran variables para que hagan +,-,* y / y se mandan a imprimir todas juntas en esta linea 
+
+#Calcular exponentes 
+exponente = 5**2 #aqui se declara una varible luego se le da el valor de 5 y con los dos ** despues se pone el numero a lo que lo quieras elevar en este caso es 2 
+print(exponente)
+
+#Float y ROUND
+numero1 = 150.39
+numero2 = 50.99
+resultado = numero1*numero2 
+print(round(resultado,2)) #roud reduce el numero de decimales que quires mostrar en el resultado donde despues del resultado el ,2 es el numero de decimales que quieres mostrar 
