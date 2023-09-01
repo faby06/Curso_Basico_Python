5492f1e84b1fd3c9268382232211f706dd9451ca
faby06 <143236825+faby06@users.noreply.github.com>
2023-09-01 12:42:49 -0600

    Update _9_Comparar_datos_listasVStuplas_conIF.py

--- _9_Comparar_datos_listasVStuplas_conIF.py
+++ _9_Comparar_datos_listasVStuplas_conIF.py
@@ -1,7 +1,7 @@
 #Comparar Datos en listas y tublas con IF
 materia=input("Introduce una materia:\n")
 nombre=['Matematicas','Historia','Electronica','Dinamica','PLCS']
-if materia in nombre:
+if materia in nombre: #Aqui dice que si materia esta dentro de la lista nombre haga el print si no con el else va mandar otro print
     print("Tienes esta materia este semestre")
 else:
     print("Esta materia no la tienes este semestre")
