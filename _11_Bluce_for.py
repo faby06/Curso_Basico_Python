d0923e1f18462f14f94e93677c11e100406e8e54
faby06 <143236825+faby06@users.noreply.github.com>
2023-09-02 10:11:19 -0600

    Add files via upload

---
+++ _11_Bluce_for.py
@@ -0,0 +1,13 @@
+#Blucle for 
+marcas=['APPLE','LENOVO','HP','HUAWEI','ASUS'] 
+for x in marcas:  #Se esta utilizando para recorrer una lista y se cumple se manda imprimir
+    if x== 'HP':
+        print(x)
+
+#Uso Range
+for a in range(5): #rango incrementa de uno en uno hasta que llega al 5  
+   print('El resultado es:',a) 
+for s in range(2,6): #Va empezar en el 2 y terminara hasta el 6 
+   print('El resultado dos es:',s)
+for r in range(0,12,2):#Inicia en el 0 y va incrementar de dos en dos hasta el 12 
+    print('El resultado tres es:',r)
