69eb4494ab09a948b90f6e70f790f8fb831b8ac0
faby06 <143236825+faby06@users.noreply.github.com>
2023-09-02 13:40:54 -0600

    Update _15_Funciones_arg.py

--- _15_Funciones_arg.py
+++ _15_Funciones_arg.py
@@ -2,7 +2,7 @@
 def multi(multi1, multi2): #Usando def es como se crean las funciones 
     result = multi1 * multi2
     return result
-# Llama a la función multi con diferentes números de argumentos
+# Llama a la funciÃ³n multi con diferentes nÃºmeros de argumentos
 result1 = multi(5, 6)
 result2 = multi(10, 8)
 result3 = multi(60, 90)
@@ -12,12 +12,12 @@ print("Resultado 2:", result2)
 print("Resultado 3:", result3)
 
 def suma(*args):
-    result = 0  # Inicializamos el resultado en 1
+    result = 0  # Inicializamos el resultado en 0
     for num in args:
         result += num
     return result
 
-# Llama a la función multi con diferentes números de argumentos
+# Llama a la funciÃ³n multi con diferentes nÃºmeros de argumentos
 resultado1 = suma(9, 6)
 resultado2 = suma(25,5)
 resultado3 = suma(100,50)
