b33959de52f4b583f2873686c22b546f59d953d7
faby06 <143236825+faby06@users.noreply.github.com>
2023-09-17 20:34:50 -0600

    Add files via upload

---
+++ 03_Listas_Actualizar.py
@@ -0,0 +1,4 @@
+
+#Crear listas y Actualizarlas 
+cel = ['Nokia','Huawei','Samsung','Xiaomi'] #Aqui estoy creando una lista de marcas de celulares 
+print (cel[3],cel[1])#Cuando mando imprimir mi nombre de marca en este caso el nombre que este en la posicion 3 lo va imprimir
