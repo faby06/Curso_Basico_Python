93e45c5bd8ba44b3cff7b4f022872cd06310ca48
faby06 <143236825+faby06@users.noreply.github.com>
2023-09-17 20:38:09 -0600

    Add files via upload

---
+++ 09_Comparar_datos_listasVStuplas_conIF.py
@@ -0,0 +1,8 @@
+#Comparar Datos en listas y tublas con IF
+materia=input("Introduce una materia:\n")
+nombre=['Matematicas','Historia','Electronica','Dinamica','PLCS']
+if materia in nombre:
+    print("Tienes esta materia este semestre")
+else:
+    print("Esta materia no la tienes este semestre")
+    
