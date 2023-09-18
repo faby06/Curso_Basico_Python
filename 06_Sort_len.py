2889d36e5f131cbfdc8835da91eb65ed21877d34
faby06 <143236825+faby06@users.noreply.github.com>
2023-09-17 20:36:12 -0600

    Add files via upload

---
+++ 06_Sort_len.py
@@ -0,0 +1,11 @@
+#Ordenar elementos sort()
+color=['Verde','Amarillo','Morado','Plata']
+color.sort()#ordena alfabeticamente la lista
+print("El orden alfabeticamente es:",(color))
+color.sort(reverse=True)#Para que ponga el alfabeto al reves
+print("El orden invertido es:",(color))
+
+#Contar elementos usando len()
+color=['Verde','Amarillo','Morado','Plata']
+print ('Cuantos elementos son en la lista: ',len(color)) #len cuenta el numero de elementos o colores que hay en esta lista
+
