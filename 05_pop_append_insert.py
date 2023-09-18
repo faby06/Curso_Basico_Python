aa12297cb499c853fa64df40d8eeff2233de1cea
faby06 <143236825+faby06@users.noreply.github.com>
2023-09-17 20:35:39 -0600

    Add files via upload

---
+++ 05_pop_append_insert.py
@@ -0,0 +1,18 @@
+#Eliminar elementos pop()
+color=['Amarillo','Verde','Azul']
+almacenar=color.pop(1)#Elimina elementos pero los puede almacenar en una variable 
+print ("El color guardado es: ",(almacenar))
+print(color)#aqui se va apreciar el color eliminado 
+
+#Insertar elementos append()
+
+colo=['Amarillo','Verde','Azul']
+color1=colo.append("Morado")#Elimina elementos pero los puede almacenar en una variable 
+print (colo)
+
+#Insertar elementos insert()
+color12=['Amarillo','Verde','Azul']
+color12.insert(0,'Negro') #Con ayuda de este elemento se inserta otro color dependiendo en la posicion que yo elija en este caso se va mostrar adelante del amarillo
+print(color12)
+
+
